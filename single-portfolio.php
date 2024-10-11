dfadde9483401032a5f9676fd0bce9de98a5b5f2
Théo Feschotte <theofeschotte@gmail.com>
2024-10-11 13:45:32 +0200

    Update single-portfolio.php

--- single-portfolio.php
+++ single-portfolio.php
@@ -3,4 +3,25 @@
 $context = Timber::context();
 $context['post'] = Timber::get_post();
 
+$context['posts'] = Timber::get_posts([
+    'post_type' => 'portfolio',
+    'post__not_in' => [$context['post']->ID],
+    'posts_per_page' => 2,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'tax_query' => [
+        'relation' => 'OR',
+        [
+            'taxonomy' => 'categorie',
+            'field' => 'slug',
+            'terms' => wp_get_post_terms($context['post']->ID, 'categorie', ['fields' => 'slugs'])[0],
+        ],
+        [
+            'taxonomy' => 'format',
+            'field' => 'slug',
+            'terms' => wp_get_post_terms($context['post']->ID, 'format', ['fields' => 'slugs'])[0],
+        ],
+    ],
+]);
+
 Timber::render( 'layouts/single-portfolio.twig', $context );
