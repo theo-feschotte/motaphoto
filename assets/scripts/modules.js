9b153a569af268064ba59ac0976f1cb1d508cdec
Théo Feschotte <theofeschotte@gmail.com>
2024-11-15 15:59:12 +0100

    Création Modal.js

--- assets/scripts/modules.js
+++ assets/scripts/modules.js
@@ -2,6 +2,7 @@ export { default as Ajax } from "./modules/Ajax";
 export { default as Load } from "./modules/Load";
 export { default as Scroll } from "./modules/Scroll";
 export { default as Menu } from "./modules/Menu";
+export { default as Modal } from "./modules/Modal";
 // export { default as Text } from "./modules/Text";
 // export { default as Split } from "./modules/SplitTexts";
 // export { default as Slider } from "./modules/Slider";
---
+++ assets/scripts/modules/Modal.js
@@ -0,0 +1,49 @@
+import { module as BaseModule } from "modujs";
+
+export default class extends BaseModule {
+    constructor(m) {
+        super(m);
+
+        this.events = {
+            click: {
+              btnContact: "openModal",
+              close: "closeModal",
+            },
+        };
+
+        this.container = this.$("container")[0];
+        this.delay = '200';
+    }
+    
+    init() {
+        console.log("Modal init");
+
+        const customDuration = Array.from(this.container.classList).some(className => className.includes('duration-['));
+        const tailwindDuration = Array.from(this.container.classList).some(className => className.includes('duration-'));
+        if (customDuration) {
+            this.delay = Array.from(this.container.classList).find(className => className.includes('duration-[')).split('duration-[')[1].split('s]')[0];
+            this.delay = this.delay * 1000;
+        }
+        if (!customDuration && tailwindDuration) {
+            this.delay = Array.from(this.container.classList).find(className => className.includes('duration-')).split('duration-')[1];
+        }
+    }
+
+    openModal() {
+        this.container.classList.remove("hidden");
+        setTimeout(() => {
+            this.container.classList.remove("opacity-0");
+            this.container.classList.remove("user-none");
+            this.container.classList.remove("pointer-events-none");
+        }, 0);
+    }
+
+    closeModal() {
+        this.container.classList.add("opacity-0");
+        this.container.classList.add("user-none");
+        this.container.classList.add("pointer-events-none");
+        setTimeout(() => {
+            this.container.classList.add("hidden");
+        }, this.delay);
+    }
+}
